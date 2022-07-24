aca4cdc0c926063361b49f2d8375a65156651e52
arriven <20084245+Arriven@users.noreply.github.com>
2022-07-24 12:16:39 +0300

    implement parsing (hopefully)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, AsyncBufReadExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::stream::StreamExt;
 
@@ -20,14 +20,156 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
-async fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+async fn handle_connection(stream: TcpStream) -> io::Result<()> {
+    let stream = tokio::io::BufStream::new(stream);
+    let mut processor = Processor::new(stream);
+    while let Ok(()) = processor.process_message().await {
+        // let response = "+PONG\r\n";
 
-    while let Ok(_) = stream.read(&mut buffer).await {
-        let response = "+PONG\r\n";
+        // stream.write(response.as_bytes()).await.unwrap();
+        // stream.flush().await.unwrap();
+    }
+    Ok(())
+}
 
-        stream.write(response.as_bytes()).await.unwrap();
-        stream.flush().await.unwrap();
+enum Value {
+    Nil,
+    Int(i64),
+    String(String),
+    Array(usize, Vec<Value>)
+}
+
+impl Value {
+    fn is_complete(&self) -> bool {
+        match &self {
+            Value::Array(size, data) => if *size == data.len() {
+                data.iter().all(|val| val.is_complete())
+            } else {
+                false
+            }
+            _ => true
+        }
+    }
+
+    fn append(&mut self, val: Value) {
+        assert!(!self.is_complete());
+
+        if let Value::Array(size, ref mut data) = self {
+            if let Some(ref mut child) = data.iter_mut().find(|x| !x.is_complete()) {
+                child.append(val);
+            } else {
+                assert!(data.len() < *size);
+                data.push(val);
+            }
+        } else {
+            assert!(false); // if should always pass
+        }
+    }
+
+    fn to_command(self) -> Command {
+        match self {
+            Value::Array(_, mut data) => {
+                assert!(data.len() > 0);
+                match &data[0] {
+                    Value::String(command) => match command.as_str() {
+                        "PING" => Command::Ping,
+                        "ECHO" => {
+                            if let Value::String(data) = data.pop().unwrap() {
+                                Command::Echo(data)
+                            } else {
+                                Command::Error
+                            }
+                        },
+                        _ => Command::Error,
+                    },
+                    _ => Command::Error,
+                }
+            },
+            Value::String(data) => {
+                match data.as_str() {
+                    "PING" => Command::Ping,
+                    _ => Command::Error,
+                }
+            },
+            _ => Command::Error,
+        }
+    }
+}
+
+enum Command {
+    Error,
+    Ping,
+    Echo(String),
+}
+
+struct Processor<R> where R: tokio::prelude::AsyncRead + tokio::prelude::AsyncBufRead + tokio::prelude::AsyncWrite + std::marker::Unpin {
+    stream: R,
+}
+
+impl<R> Processor<R> where R: tokio::prelude::AsyncRead + tokio::prelude::AsyncBufRead + tokio::prelude::AsyncWrite + std::marker::Unpin {
+    fn new(stream: R) -> Processor<R> {
+        Processor{
+            stream: stream,
+        }
+    }
+
+    async fn process_message(&mut self)-> io::Result<()> {
+        let message = self.read_message().await?;
+        let command = message.to_command();
+        match command {
+            Command::Ping => {
+                let response = "+PONG\r\n";
+
+                self.stream.write(response.as_bytes()).await?;
+                self.stream.flush().await?;
+            }
+            _ => ()
+        }
+        Ok(())
+    }
+
+    async fn read_message(&mut self) -> io::Result<Value> {
+        let mut message = self.read_single().await?;
+        while !message.is_complete() {
+            let next = self.read_single().await?;
+            message.append(next);
+        }
+        Ok(message)
+    }
+
+    async fn read_single(&mut self) -> io::Result<Value> {
+        let b = self.stream.read_u8().await?;
+        match b as char {
+            '*' => {
+                let mut buf = vec![];
+                self.stream.read_until('\r' as u8, &mut buf).await?;
+                assert!(self.stream.read_u8().await? == '\n' as u8);
+                let size = buf.iter().map(|b| *b as char).collect::<String>().parse::<usize>().unwrap();
+                Ok(Value::Array(size, Vec::with_capacity(size)))
+            }
+            '$' => {
+                let mut buf = vec![];
+                self.stream.read_until('\r' as u8, &mut buf).await?;
+                assert!(self.stream.read_u8().await? == '\n' as u8);
+                let size = buf.iter().map(|b| *b as char).collect::<String>().parse::<i64>().unwrap();
+                if size > 0 {
+                    let size = size as usize;
+                    let mut result = vec![0; size];
+                    self.stream.read_exact(&mut result).await?;
+                    Ok(Value::String(result.iter().map(|b| *b as char).collect::<String>()))
+                } else {
+                    Ok(Value::Nil)
+                }
+            }
+            ':' => {
+                let mut buf = vec![];
+                self.stream.read_until('\r' as u8, &mut buf).await?;
+                assert!(self.stream.read_u8().await? == '\n' as u8);
+                let result = buf.iter().map(|b| *b as char).collect::<String>().parse::<i64>().unwrap();
+                Ok(Value::Int(result))
+            }
+            _ => Ok(Value::Nil)
+        }
     }
 }
 
